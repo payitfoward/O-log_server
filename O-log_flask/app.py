a82e0168ff35290baa988833987ca0890ec79822
백채연 <codusl100@naver.com>
2023-02-23 02:53:59 +0900

    feat : bloggpt router add

--- O-log_flask/app.py
+++ O-log_flask/app.py
@@ -1,6 +1,4 @@
-from flask import Flask
-import jsonify
-import requests
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -11,6 +9,11 @@ def home():
 @app.route('/test', methods = ['GET'])
 def test():
     return "Success"
+ 
+@app.route('/api/bloggpt', methods = ['POST'])
+def getBlog() :
+       blog = request.get_json()
+       return jsonify(blog)
 
 if __name__ == '__main__':  
    app.run('0.0.0.0', port=5000, debug=True)
