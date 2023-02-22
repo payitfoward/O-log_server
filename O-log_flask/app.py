46d142703da5362b074ac47de194ebb1d9493965
백채연 <codusl100@naver.com>
2023-02-23 02:40:27 +0900

    flask server init

---
+++ O-log_flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from url_crawling import select_area
+import jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return 'This is Home!'
+
+@app.route('/test', methods = ['GET'])
+def test():
+    return "Success"
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0', port=5000, debug=True)
